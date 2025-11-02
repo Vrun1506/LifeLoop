695ad02c0485fd1f5bc432c699532ab53ad1b0bb
Krishna Rajamannar <rajamannar@hotmail.com>
2025-11-02 01:07:19 +0000

    fixed login and sign up page UI

--- frontend/life-loop/app/login/page.tsx
+++ frontend/life-loop/app/login/page.tsx
@@ -108,9 +108,9 @@ export default function Login() {
               <button
                 type="button"
                 onClick={handleGoogleAuth}
-                className="w-full rounded-md bg-black px-6 py-2.5 text-lg font-medium text-white transition-colors hover:bg-white hover:text-black flex items-center justify-center gap-2"
+                className="w-full rounded-md bg-white px-6 py-2.5 text-lg font-medium text-black transition-colors hover:bg-white hover:text-black flex items-center justify-center gap-2 cursor-pointer"
               >
-                <svg className="w-5 h-5" viewBox="0 0 24 24">
+                <svg className="w-4 h-4" viewBox="0 0 24 24">
                   <path fill="currentColor" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
                   <path fill="currentColor" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
                   <path fill="currentColor" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/>
@@ -119,13 +119,10 @@ export default function Login() {
                 Login with Google  
               </button>
             </div>
-            <div className="space-y-2 mt-6">
-              <a
-                href="/register"
-                className="block w-full text-center rounded-md bg-transparent border-2 border-white px-6 py-2.5 text-lg font-medium text-white transition-colors hover:bg-white hover:text-black"
-              >
-                Don't have an account? Sign up  
-              </a>
+            <div className="space-y-2 mt-6 justify-center text-center">
+             <p className="mt-6 text-base  text-[#FCF7F8]">
+                Don't have an account? <a href="/register" className="underline hover:text-black">Sign up</a>        
+            </p>
             </div>
           </form>
         </div>
